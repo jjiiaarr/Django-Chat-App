e5db373bb51c6bfcc880457b2c27f5aaa7d55b5a
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-17 16:23:04 +0800

    create views for room model and urls on it

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from chat.models import Room, Message
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def room(request, room):
+    return render(request, 'room.html')
+
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect(f'/{room}/?username={username}')
+
+    new_room = Room.objects.create(name=room)
+    new_room.save()
